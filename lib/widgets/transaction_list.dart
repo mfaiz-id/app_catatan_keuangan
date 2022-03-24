b177aa882c2305aa98afa09115ed17daf69eb971
Moch Faizal Ansyori <faizmuhammad810@gmail.com>
2022-03-24 21:29:02 +0700

    scroll view

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,50 +11,55 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      ...transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.fromLTRB(10, 15, 10, 15),
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  ),
-                ),
-                child: Text(
-                  '\$${tx.amount}',
-                  style: const TextStyle(
-                    color: Colors.purple,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 350,
+      child: SingleChildScrollView(
+        child: Column(children: [
+          ...transactions.map((tx) {
+            return Card(
+              child: Row(
                 children: [
-                  Text(
-                    tx.title,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(10, 15, 10, 15),
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    child: Text(
+                      '\$${tx.amount}',
+                      style: const TextStyle(
+                        color: Colors.purple,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                  Text(
-                    DateFormat().format(tx.time),
-                    style: const TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        DateFormat().format(tx.time),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-        );
-      })
-    ]);
+              ),
+            );
+          })
+        ]),
+      ),
+    );
   }
 }
